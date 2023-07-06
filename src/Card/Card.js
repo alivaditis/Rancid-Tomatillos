a42499d1d347fdd7492b9e405169792d3202d765
Alec Livaditis <aleclivaditis@gmail.com>
2023-07-06 19:08:04 -0400

    display tomato conditionally

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './Card.css'
+import slime from '../imgs/green-slime.png'
+import tomato from '../imgs/tomato.png'
 
 function Card({id, posterSrc, rating, releaseDate, title}){
   return (
     <div className='card' id={id}>
       <div className='poster' style={{ backgroundImage: `url(${posterSrc})` }}></div>
       <section className='rating'>
-        <img src={rating < 6 ? "../src/imgs/green-slime.png" : "../src/imgs/green-slime.png"}/> {Math.floor(rating*10)}%
+        <img class='tomato' src={rating < 6 ? slime : tomato}/> {Math.floor(rating*10)}%
       </section>
       <p className="title">{title}</p>
     </div>
