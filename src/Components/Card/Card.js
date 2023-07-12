aee48c7683654db0de0f1e0a1a1ff879d8106500
Benjamin holden <benjamin.f.holden@outlook.com>
2023-07-11 20:48:54 -0600

    incomplete/Router

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -4,31 +4,31 @@ import {getMovie} from '../../api'
 import './Card.css'
 import slime from '../../imgs/green-slime.png'
 import tomato from '../../imgs/tomato.png'
+import { Link } from 'react-router-dom';
 
 function Card({id, posterSrc, rating, title, updateSelectedMovie}) {
 
  
   
   return (
-    <div
-      className='card'
-      id={id}
-      onClick={updateSelectedMovie}
-      >
-      <div 
-        className='poster' 
-        style={{ backgroundImage: `url(${posterSrc})` }}
-        onClick={updateSelectedMovie}>
+    <Link to={`/${id}`}>
+      <div
+        className='card'
+        id={id}
+        >
+        <div 
+          className='poster' 
+          style={{ backgroundImage: `url(${posterSrc})` }}>
+        </div>
+        <div className='wrap'>
+          <section className='rating'>
+            <img className='tomato' src={rating < 6 ? slime : tomato} /> 
+            <span>{Math.floor(rating*10)}%</span>
+          </section>
+          <p className="title">{title} </p>
+        </div>
       </div>
-      <div className='wrap'>
-        <section className='rating'>
-          <img className='tomato' src={rating < 6 ? slime : tomato} onClick={updateSelectedMovie}/> 
-          <span>{Math.floor(rating*10)}%</span>
-        </section>
-        <p className="title" onClick={updateSelectedMovie}>{title} </p>
-      </div>
-    </div>
-  )
+    </Link>)
 }
 
 export default Card
--- src/Components/SelectedMovie/SelectedMovie.js
+++ src/Components/SelectedMovie/SelectedMovie.js
@@ -2,43 +2,65 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import slime from '../../imgs/green-slime.png'
 import tomato from '../../imgs/tomato.png'
-import {getTrailerKey} from '../../api'
+import {getTrailerKey, getMovie} from '../../api'
 import './SelectedMovie.css'
+import { useParams } from 'react-router-dom'
 
-function SelectedMovie({selectedMovie, setMovieView, setSelectedMovie}){
-
+function SelectedMovie(){
+  const [userMovie, setUserMovie] = useState({})
   const [trailerKey, setTrailerKey] = useState('')
+  const {id} = useParams()
+  // console.log(id)
+  useEffect( () => {  
+    getMovie(id)
+    .then(data => {
+      console.log(data.movie, "Data.movie")
+      setUserMovie(data.movie)
+      console.log(userMovie, "SelectedMovie")
+    })
+    }, [id])
+      // if (!data) {
+      //   setServerError(true)
+      // } else {
+        
+      // }
+    // })
+    // .then(x => setMovieView(true))
+  
 
   useEffect(() => {
-    getTrailerKey(selectedMovie.id)
+    getTrailerKey(userMovie.id)
       .then(key => setTrailerKey(key))
   }, [])
 
   return (
-  <div className='selectedMovie'>
-    {trailerKey && <iframe className='trailer' src= {`https://www.youtube.com/embed/${trailerKey}`}></iframe>}
-    {!trailerKey && <img className='backdrop' src={selectedMovie.backdrop_path}/>}
-    <div className='container'>
-      <img className='selectedPoster' src={selectedMovie.poster_path}/>
-      <div className='information'>
-        <h3 className='selectedTitle'>{selectedMovie.title}</h3>
-        <p>
-          <span>{selectedMovie.release_date.slice(0,4)}, </span>
-          <span>{selectedMovie.genres.join(' / ')}, </span>
-          <span>{Math.floor(selectedMovie.runtime/60)}h {selectedMovie.runtime % 60}m</span>
-        </p>
-        <div className='rating-container'>
-          <img className='rating-img' src={selectedMovie.average_rating < 6 ? slime : tomato} />
-          <span className='percentage'>{Math.floor(selectedMovie.average_rating*10)}%</span>
+    <div>
+      {/* {userMovie && */}
+      <div className='selectedMovie'>
+      {trailerKey && <iframe className='trailer' src= {`https://www.youtube.com/embed/${trailerKey}`}></iframe>}
+      {!trailerKey && <img className='backdrop' src={userMovie.backdrop_path}/>}
+      <div className='container'>
+        <img className='selectedPoster' src={userMovie.poster_path}/>
+        <div className='information'>
+          <h3 className='selectedTitle'>{userMovie.title}</h3>
+          <p>
+            <span>{userMovie.release_date.slice(0,4)}, </span>
+            <span>{userMovie.genres.join(' / ')}, </span>
+            <span>{Math.floor(userMovie.runtime/60)}h {userMovie.runtime % 60}m</span>
+          </p>
+          <div className='rating-container'>
+            <img className='rating-img' src={userMovie.average_rating < 6 ? slime : tomato} />
+            <span className='percentage'>{Math.floor(userMovie.average_rating*10)}%</span>
+          </div>
         </div>
       </div>
+      <p className='overview'>{userMovie.overview}</p>
+      <button className='backButton' onClick={() => {
+        // setSelectedMovie({})
+        }}>Back</button>
+      </div>
+      {/* } */}
     </div>
-    <p className='overview'>{selectedMovie.overview}</p>
-    <button className='backButton' onClick={() => {
-      setMovieView(false) 
-      setSelectedMovie({})
-      }}>Back</button>
-  </div>
   )
 }
 
